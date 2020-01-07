114d5cc8cdcbbd26db0f02d8659c9b23f5dd64d4
B.Maher <bouzid.meher@gmail.com>
2020-01-07 18:33:33 +0100

    skeleton fone and database schema finished for now

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const db = require("../db/index.js");
 
-const app = express();
 let PORT = process.env._PORT || 5000;
-app.use(express.static(path.resolve(__dirname, '../build')))
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.resolve(__dirname, "../build")));
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+  console.log(`listening on port ${PORT}`);
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+});
