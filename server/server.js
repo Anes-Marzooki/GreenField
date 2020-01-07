20d73532eb54b6cad87fd65acf468a876cfd1d15
B.Maher <bouzid.meher@gmail.com>
2020-01-07 16:04:27 +0100

    trying first deploy

--- server/server.js
+++ server/server.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 let PORT = process.env._PORT || 5000;
+app.use(express.static(path.join(__dirname, '../build')))
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+    console.log(`listening on port ${PORT}`)
 })
 
-
-
-app.use(express.static(path.join(__dirname, 'build')))
-
-
-
-
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '../build', 'index.html'))
 })
