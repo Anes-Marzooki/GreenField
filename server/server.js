127c522b774759a9f2aa0fbcc1921d9be9f14495
B.Maher <bouzid.meher@gmail.com>
2020-01-07 16:52:04 +0100

    skeleton done

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 const app = express();
 let PORT = process.env._PORT || 5000;
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.resolve(__dirname, '../build')))
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
 })
