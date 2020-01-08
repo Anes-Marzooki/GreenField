10e9527a941acc09fcb3eae17a8329fc6150065c
Anes Marzooki <marzoukianas@gmail.com>
2020-01-08 16:40:33 +0100

    renders_list_of_teachers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { teachers } from './teachers.js';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teachers: teachers,
+    }
+
+  }
+  render() {
+    return(
+      <div>
+        <ul>
+          {
+            this.state.teachers.map( teacher => {
+              <li key={teacher.id}>
+                <h2> {teacher.name} </h2>
+                <h2> {teacher.category} </h2>
+                <h3> {teacher.email} </h3>
+              </li>
+            })
+          }
+        </ul>
+      </div>
+      )
+  }
 }
 
 export default App;
